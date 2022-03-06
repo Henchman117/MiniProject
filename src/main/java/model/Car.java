c5da88dda620d856bdba04b8d26a8f1acd51d737
osminer00 <90017859+osminer00@users.noreply.github.com>
2022-03-05 22:56:10 -0600

    Added specifications object to car

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -21,9 +21,27 @@ public class Car {
 	private String model;
 	@Column(name = "MANUFACTURER")
 	private String manufacturer;
+	private Specifications specs;
 	
 	
-	
+	/**
+	 * @return the specs
+	 */
+	public Specifications getSpecs() {
+		return specs;
+	}
+	/**
+	 * @param specs the specs to set
+	 */
+	public void setSpecs(Specifications specs) {
+		this.specs = specs;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
 	public int getId() {
 		return id;
 	}
